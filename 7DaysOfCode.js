7f28874a0a441da3d9f0c51ae9da85e8d583f2f4
Girckus <rodrigogirckus@gmail.com>
2023-02-17 20:01:03 -0300

    Dia 2 - 7DaysOfCode - JavaScript

--- 7DaysOfCode.js
+++ 7DaysOfCode.js
@@ -30,3 +30,25 @@ if (numeroDez == stringDez) {
 } else {
     console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
 }
+
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
+
+document.querySelector("#nome").textContent = nome;
+document.querySelector("#idade").textContent = idade;
+document.querySelector("#linguagem").textContent = linguagem;
+
+let gosta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`);
+
+while(gosta != 1 && gosta != 2) {
+    gosta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`);
+}
+
+if(gosta == 1) {
+    alert(`Muito bom! Continue estudando ${linguagem} e você terá muito sucesso.`);
+}
+
+if(gosta == 2) {
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+}
